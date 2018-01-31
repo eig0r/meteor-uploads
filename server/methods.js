96630858d29388507dfcbef2fb077d515a0264da
eig0r <eigor.node@gmail.com>
2018-01-31 15:54:18 +0200

    Delete methods.js

--- server/methods.js
+++
@@ -1,9 +0,0 @@
-Meteor.methods({
-	toggleAdmin(id) {
-		if(Roles.userIsInRole(id, 'admin')) {
-			Roles.removeUsersFromRoles(id, 'admin');
-		} else {
-			Roles.addUsersToRoles(id, 'admin');
-		}		
-	}
-})
